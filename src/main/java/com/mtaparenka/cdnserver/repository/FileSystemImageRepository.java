b3ea790b69ba6d1cfc57a7a8cfd7da8f9a9625b5
maxtopor <maxtoporenko@gmail.com>
2024-08-30 00:59:49 +0200

    Read jpeg metadata POC

--- src/main/java/com/mtaparenka/cdnserver/repository/FileSystemImageRepository.java
+++ src/main/java/com/mtaparenka/cdnserver/repository/FileSystemImageRepository.java
@@ -4,11 +4,11 @@ import com.mtaparenka.cdnserver.config.ContentProperties;
 import com.mtaparenka.cdnserver.model.UploadData;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 @Component
 public class FileSystemImageRepository implements ImageRepository {
@@ -23,11 +23,36 @@ public class FileSystemImageRepository implements ImageRepository {
         var file = new File(contentProperties.images().path() + "/" + fileName);
 
         try (var stream = new FileInputStream(file)) {
-            return stream.readAllBytes();
+            var bytes = stream.readAllBytes();
+            readMetadata(bytes);
+            return bytes;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
+    }
+
+    private void readMetadata(byte[] bytes) {
+        int app1Start = 0;
+        int i = 0;
+        var hexes = new String[bytes.length];
+
+        for (int j = 0; j < bytes.length; j++) {
+            hexes[j] = String.format("%02x", bytes[j]);
+        }
+
+        while (app1Start == 0) {
+            if (Byte.toUnsignedInt(bytes[i]) == 0xff && Byte.toUnsignedInt(bytes[i+1]) == 0xe1) {
+                app1Start = i;
+            }
+            i++;
+        }
+
+        int app1Length = ((bytes[app1Start + 2] & 0xff) << 8) | (bytes[app1Start + 3] & 0xff);
+
+
+        var string = new String(Arrays.copyOfRange(bytes, app1Start, app1Length), StandardCharsets.UTF_8);
+        System.out.println(string);
     }
 
     @Override
