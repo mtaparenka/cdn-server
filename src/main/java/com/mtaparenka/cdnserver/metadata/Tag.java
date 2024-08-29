70793dbb22c37178ee4568fe90c3fe5499b46c28
maxtopor <maxtoporenko@gmail.com>
2024-08-30 00:59:49 +0200

    Read jpeg metadata POC

---
+++ src/main/java/com/mtaparenka/cdnserver/metadata/Tag.java
@@ -0,0 +1,17 @@
+package com.mtaparenka.cdnserver.metadata;
+
+public enum Tag {
+    MAKE(0x010f, Type.ASCII),
+    MODEL(0x0110, Type.ASCII),
+    DATE_TIME(0x0132, Type.ASCII),
+    ARTIST(0x013b, Type.ASCII),
+    IMAGE_DESCRIPTION(0x010e, Type.ASCII);
+
+    public final int address;
+    public final Type type;
+
+    Tag(int address, Type type) {
+        this.address = address;
+        this.type = type;
+    }
+}
--- src/main/java/com/mtaparenka/cdnserver/repository/FileSystemImageRepository.java
+++ src/main/java/com/mtaparenka/cdnserver/repository/FileSystemImageRepository.java
@@ -1,14 +1,17 @@
 package com.mtaparenka.cdnserver.repository;
 
 import com.mtaparenka.cdnserver.config.ContentProperties;
+import com.mtaparenka.cdnserver.metadata.JpegMetadata;
 import com.mtaparenka.cdnserver.model.UploadData;
 import org.springframework.stereotype.Component;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.UUID;
-import java.util.stream.Stream;
 
 @Component
 public class FileSystemImageRepository implements ImageRepository {
@@ -24,35 +27,12 @@ public class FileSystemImageRepository implements ImageRepository {
 
         try (var stream = new FileInputStream(file)) {
             var bytes = stream.readAllBytes();
-            readMetadata(bytes);
+            var metadata = JpegMetadata.read(bytes);
             return bytes;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-    }
-
-    private void readMetadata(byte[] bytes) {
-        int app1Start = 0;
-        int i = 0;
-        var hexes = new String[bytes.length];
-
-        for (int j = 0; j < bytes.length; j++) {
-            hexes[j] = String.format("%02x", bytes[j]);
-        }
-
-        while (app1Start == 0) {
-            if (Byte.toUnsignedInt(bytes[i]) == 0xff && Byte.toUnsignedInt(bytes[i+1]) == 0xe1) {
-                app1Start = i;
-            }
-            i++;
-        }
-
-        int app1Length = ((bytes[app1Start + 2] & 0xff) << 8) | (bytes[app1Start + 3] & 0xff);
-
-
-        var string = new String(Arrays.copyOfRange(bytes, app1Start, app1Length), StandardCharsets.UTF_8);
-        System.out.println(string);
     }
 
     @Override
